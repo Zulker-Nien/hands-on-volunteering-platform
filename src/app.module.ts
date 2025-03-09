033f79bedb46eb03cce0ef12d4136602223d53e0
Zulker Nien <zulkerb9b@gmail.com>
2025-03-09 22:16:13 +0600

    feat: Added TyprORM and Config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,37 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
+import enviromentValidation from './config/enviroment.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
+const ENV = process.env.NODE_ENV;
+
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      // envFilePath: ['.env.development', '.env'],
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: enviromentValidation,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: configService.get('database.synchronize'),
+        port: configService.get('database.port'),
+        username: configService.get('database.user'),
+        password: configService.get('database.password'),
+        host: configService.get('database.host'),
+        autoLoadEntities: configService.get('database.autoLoadEntities'),
+        database: configService.get('database.name'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
